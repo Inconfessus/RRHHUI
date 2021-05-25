5367c973376cf7866712c47b5456e08d46f3a282
Drablaguna <drablaguna@gmail.com>
2021-05-25 13:19:00 -0500

    front back 5

--- js/buscar.js
+++ js/buscar.js
@@ -22,15 +22,12 @@ function init() {
 function buscar() {
     var name = document.getElementById('input-name').value;
     var last_name = document.getElementById('input-last_name').value;
-    var full_name = name + last_name
 
     axios.post(url + "/empleados/buscar", { name: name, last_name: last_name },
         headers).then(function(res) {
-        console.log(res.data.message);
         displayEmpleados(res.data.message)
     }).catch(function(err) {
         alert("No se pudieron obtener los datos del empleado")
-        console.log(full_name)
     })
 }
 
@@ -43,7 +40,8 @@ function displayEmpleados(empleados) {
         //                       <h3>Teléfono: ${empleados[i].phone}</h3><br>
         //                       <h3>Correo: ${empleados[i].email}</h3><br>
         //                       <h3>Dirección: ${empleados[i].address}</h3><br>`;
-        document.getElementById("nombre").innerHTML = empleados[i].name
+        document.getElementById("nombre").innerHTML = empleados[i].name + ' ' + empleados[i].last_name
+        document.getElementById("idx").innerHTML = empleados[i].user_id
         document.getElementById("telefono").innerHTML = empleados[i].phone
         document.getElementById("email").innerHTML = empleados[i].email
         document.getElementById("direccion").innerHTML = empleados[i].address
